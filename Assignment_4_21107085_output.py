9e4c0b895a81dd08bc68f496239f267620afa154
DipanshuPanda <103227279+DipanshuPanda@users.noreply.github.com>
2022-05-25 22:16:50 +0530

    Add files via upload

---
+++ Assignment_4_21107085_output.py
@@ -0,0 +1,54 @@
+'''Name- Dipanshu Panda
+SID - 21107085
+Branch- Mechanical'''
+# Q1.
+PS C:\Users\Dipanshu\Desktop\Python_PEC> & C:/Users/Dipanshu/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Dipanshu/Desktop/Python_PEC/Assignment_4_21107085.py
+Enter your marks: 23
+Grade obtained is:F
+exit?(y/n)n
+Enter your marks: 45
+Grade obtained is:D
+exit?(y/n)n
+Enter your marks: 90
+Grade obtained is:A
+exit?(y/n)y
+PS C:\Users\Dipanshu\Desktop\Python_PEC>
+
+
+# Q2.
+PS C:\Users\Dipanshu\Desktop\Python_PEC> & C:/Users/Dipanshu/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Dipanshu/Desktop/Python_PEC/Assignment_4_21107085.py
+Enter a year: 4500
+The year you entered is a leap year
+PS C:\Users\Dipanshu\Desktop\Python_PEC>
+
+
+# Q3.
+PS C:\Users\Dipanshu\Desktop\Python_PEC> & C:/Users/Dipanshu/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Dipanshu/Desktop/Python_PEC/Assignment_4_21107085.py
+Lets solve!!!
+Question.1: 9X2 = 18
+Congratulations you are correct
+Question.2: 1X4 = 5
+Sorry child the correct answer is 4
+Question.3: 0X1 = 0
+Congratulations you are correct
+Question.4: 2X2 = 4
+Congratulations you are correct
+Question.5: 2X0 = 0
+Congratulations you are correct
+Question.6: 2X4 = 3
+Sorry child the correct answer is 8
+Question.7: 0X2 = 0
+Congratulations you are correct
+Question.8: 5X6 = 30
+Congratulations you are correct
+Question.9: 3X6 = 18
+Congratulations you are correct
+Question.10: 8X1 = 8
+Congratulations you are correct
+PS C:\Users\Dipanshu\Desktop\Python_PEC> PS
+
+
+# Q4.
+PS C:\Users\Dipanshu\Desktop\Python_PEC> & C:/Users/Dipanshu/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Dipanshu/Desktop/Python_PEC/Assignment_4_21107085.py
+177 is the possible number of candys
+PS C:\Users\Dipanshu\Desktop\Python_PEC>
