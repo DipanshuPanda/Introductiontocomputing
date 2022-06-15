75653b9cfa188111e762a03facc59f8841b3b972
DipanshuPanda <103227279+DipanshuPanda@users.noreply.github.com>
2022-06-15 23:01:30 +0530

    Add files via upload

---
+++ Assignment_5_21107085_output.py
@@ -0,0 +1,78 @@
+PS C:\Users\Dipanshu> & C:/Users/Dipanshu/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Dipanshu/Desktop/Python_PEC/Assignment_5_21107085.py
+Hey user enter a sentence: My name is Dipanshu
+uhsnapiD si eman yM
+Hey user enter upto what range you want to check the divisibility: 20
+Enter the number for which you want to check divisibility: 3
+3
+6
+9
+12
+15
+18
+Enter side 1 of triangle: 3
+Enter side 2 of triangle: 4
+Enter side 3 of triangle: 5
+Area of the given triangle is 6.0
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+Input the number of rows = 5
+A 
+B C
+D E F
+G H I J
+K L M N O
+Please, Enter the Lowest Range Value: 50
+Please, Enter the Upper Range Value: 100
+The Prime Numbers in the range are: 
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+77
+154
+231
+308
+385
+462
+Enter a number: 23
+Enter a number: -42
+Enter a number: -67
+Enter a number: 12
+Enter a number: 12
+Enter a number: 23
+Enter a number: 54
+Enter a number: 29
+Enter a number: 112
+Enter a number: 345
+
+List of all negative numbers is = [-42, -67]
+List of all positive numbers is = [23, 12, 12, 23, 54, 29, 112, 345]
+List of all even numbers is = [-42, 12, 12, 54, 112]
+List of all odd numbers is = [23, -67, 23, 29, 345]
+Integer:23 occurs 2 times
+Integer:-42 occurs 1 times
+Integer:-67 occurs 1 times
+Integer:12 occurs 2 times
+Integer:54 occurs 1 times
+Integer:29 occurs 1 times
+Integer:112 occurs 1 times
+Integer:345 occurs 1 times
+Hey user enter a list of words: ['Hello','welcome','Disneyland','Dipanshu','Dipanshu']
+Word:Hello occurs 1 times
+Word:welcome occurs 1 times
+Word:Disneyland occurs 1 times
+Word:Dipanshu occurs 2 times
+PS C:\Users\Dipanshu> 
